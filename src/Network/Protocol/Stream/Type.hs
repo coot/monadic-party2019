29e7949376e01f930f58a50ccbc5414deb0cce88
Marcin Szamotulski <profunctor@pm.me>
2019-06-18 21:33:28 +0200

    Added type signatures

--- src/Network/Protocol/Stream/Type.hs
+++ src/Network/Protocol/Stream/Type.hs
@@ -1,12 +1,17 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE EmptyCase          #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE GADTs              #-}
+{-# LANGUAGE InstanceSigs       #-}
+{-# LANGUAGE PolyKinds          #-}
+{-# LANGUAGE RankNTypes         #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE TypeFamilies       #-}
 
 
 module Network.Protocol.Stream.Type where
+
+import           Data.Void (Void)
 
 import           Network.TypedProtocol.Core
 
@@ -34,17 +39,37 @@ instance Protocol (Stream id chunk) where
          --
          MsgEndStream :: Message (Stream id chunk) 'StBusy 'StDone
 
+    -- | Singletons of states in which client has the agency.
+    --
     data ClientHasAgency st where
          TokIdle :: ClientHasAgency 'StIdle
 
+    -- | Singletons of states in which the server has the agency.
+    --
     data ServerHasAgency st where
          TokBusy :: ServerHasAgency 'StBusy
 
+    -- | Singletons of terminal states.
+    --
     data NobodyHasAgency st where
          TokDone :: NobodyHasAgency 'StDone
 
+    exclusionLemma_ClientAndServerHaveAgency :: forall (st :: Stream id chunk).
+                                                ClientHasAgency st
+                                             -> ServerHasAgency st
+                                             -> Void
     exclusionLemma_ClientAndServerHaveAgency TokIdle tok = case tok of {}
+
+    exclusionLemma_NobodyAndClientHaveAgency :: forall (st :: Stream id chunk).
+                                                NobodyHasAgency st
+                                             -> ClientHasAgency st
+                                             -> Void
     exclusionLemma_NobodyAndClientHaveAgency TokDone tok = case tok of {}
+
+    exclusionLemma_NobodyAndServerHaveAgency :: forall (st :: Stream id chunk).
+                                                NobodyHasAgency st
+                                             -> ServerHasAgency st
+                                             -> Void
     exclusionLemma_NobodyAndServerHaveAgency TokDone tok = case tok of {}
 
 deriving instance (Show id, Show chunk)
